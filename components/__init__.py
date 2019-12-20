210f0f61ad2a4fff2c1a09b2e92158e3a84b5e4b
bortnovskiy gleb <bortnovskiygleb@gmail.com>
2019-12-20 17:37:04 +0300

    delete useless line

--- components/__init__.py
+++ components/__init__.py
@@ -6,4 +6,3 @@ from . database import select_courses_by_date, select_weather_by_date,\
     insert_current_courses_in_database, select_current_courses, insert_current_movies_in_database,\
     select_current_movies, update_movies_in_database, update_weather_in_database, update_courses_in_database,\
     update_information_time, select_time, update_information_date, select_date
-from . location import get_cafes, get_pharmacies, get_atms
